ea77395feb96856d595bce1052293ffc20e89af3
Yadav Rohit O <rohit.077yadav@gmail.com>
2022-06-13 10:29:41 +0530

    js files added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Fabrikam
+# Transflower Demo
+This repo contains simple Web app for hosting demo
+contains Javascript code
+Written by : Ravi Tambade
---
+++ server.js
@@ -0,0 +1,38 @@
+/*var count=34;
+count++;
+console.log("Server is running");
+console.log(count);
+
+
+//CTL + SHIFT + C   
+
+*/
+
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Chief Mentor :Ravi Tambade");
+};
+var onDefault=function(req, res){
+    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+              "<hr/>"+
+              "<ol>"+
+                    " <li>Mentoring for skill building</li>"+
+                    " <li>Workshops</li>"+
+                    " <li>Online Course</li>"+
+                    " <li>Corporate Training</li>"+
+             "</ol>");
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8081);
+console.log("Server is running on port 8081");
+// if external packages are required
+// we have to download those packages from internet using  package manager tool
+// Runtime--------tools-------------packages-----------------------config file
+// Java-----------maven--------------.jar files----------------------pom.xml
+// Android--------graddle------------android jar file----------------pom.xml
+// Python---------pip----------------Python libraries----------------
+// .NET ---------Nugget--------------dll files-----------------------package.config
+// NodeJS----------npm --------------download node modules-----------package.json 
